39471abbc2e1c67fafea8e6c1d22336e38447cd9
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-12-28 23:53:22 +0300

    a wiew added

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -6,9 +6,15 @@ namespace eTickets.Models
     public class Cinema
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
+
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
+
+        [Display(Name = "Cinema Logo")]
         public string Name { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
         public List<Movie> Movies { get; set; }
     }
