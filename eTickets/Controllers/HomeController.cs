08540cfc335573fa8c82f2cf68fe74a11faa9c9c
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2023-01-03 21:56:24 +0300

    views added

--- eTickets/Controllers/HomeController.cs
+++ eTickets/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using eTickets.Models;
 
 namespace eTickets.Controllers
 {
